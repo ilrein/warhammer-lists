83f4d43cce42bd4124b7e2cf63ba6b55f35fde13
Ilia Reingold <iliareingold@Ilias-MacBook-Pro.local>
2021-07-31 18:20:29 -0400

    init

---
+++ pages/index.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="column">
+        ++ Battalion Detachment -3CP (Chaos - Death Guard) [74 PL, 6CP, 1,510pts] ++
+        <br>
+        Battle Size [12CP]: 3. Strike Force (101-200 Total PL / 1001-2000 Points)
+        <br>
+        Detachment Command Cost [-3CP]
+        <br>
+        Plague Company: <b>Mortarion's Anvil</b>
+        <br>
+        Gifts of Decay [-2CP]: 2x Additional Relics
+        <br>
+        ++ Total: [99 PL, 7CP, 2,000pts] ++
+      </div>
+    </div>
+    <hr>
+    <div class="row">
+      <div class="column">
+        <h4>+ HQ +</h4>
+        Lord of Contagion [6 PL, -1CP, 120pts]: 3. Hulking Physique, Plaguechosen, Plaguereaper, Warp Insect Hive
+        <br>
+        Malignant Plaguecaster [5 PL, 95pts]: 1. Miasma of Pestilence, 4. Putrescent Vitality, Bolt pistol, Plague Skull of Glothila
+      </div>
+    </div>
+    <hr>
+    <div class="row">
+      <div class="column">
+        <h4>+ Troops +</h4>
+        Poxwalkers [5 PL, 100pts]
+        . 20x
+        <br>
+        Poxwalkers [2 PL, 50pts]
+        . 10x
+        <br>
+        Poxwalkers [2 PL, 50pts]
+        . 10x
+      </div>
+    </div>
+    <hr>
+
+    <div class="">
+      <h4>+ Elites +</h4>
+      <div class="">
+        Blightlord Terminators [10 PL, 210pts] Blight launcher, Flail of corruption
+      </div>
+      <br>
+      <div class="">
+        Blightlord Terminators [10 PL, 210pts] Blight launcher, Flail of corruption
+      </div>
+
+      <br>
+      <div class="">
+        Deathshroud Terminators [7 PL, -1CP, 170pts]
+        <br>
+        . Deathshroud Champion: Champion of Disease, Chimes of contagion, 2x Plaguespurt gauntlet, Reaper of Glorious Entropy
+        <br>
+        . 2x Deathshroud Terminator: 2x Manreaper, 2x Plaguespurt gauntlet
+      </div>
+      <br>
+      <div class="">
+        Foul Blightspawn [5 PL, -1CP, 85pts]: Gloaming Bloat, Plaguechosen, Viscous Death
+      </div>
+      <div class="">
+        Tallyman [4 PL, 70pts]
+      </div>
+      <br>
+    </div>
+
+    <div class="row">
+      <div class="column">
+        <h4>+ Heavy Support +</h4>
+        Plagueburst Crawler [9 PL, 175pts]: 2x Entropy cannon, Heavy slugger
+        <br>
+        Plagueburst Crawler [9 PL, 175pts]: 2x Entropy cannon, Heavy slugger
+        <br>
+      </div>
+    </div>
+    <hr>
+
+    <div class="row">
+      <div class="column">
+        <h4>+ Primarch | Daemon Primarch | Supreme Commander +</h4>
+        Mortarion [25 PL, 490pts]: 1. Miasma of Pestilence, 5. Curse of the Leper, 6. Gift of Plagues, The Droning, Warlord
+        <br>
+      </div>
+    </div>
+    <br>
+    <br>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
